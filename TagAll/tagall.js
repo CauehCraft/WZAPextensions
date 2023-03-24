0ed4948276bbc1feb1b09a285eb5a0e1288b033f
Caueh Oliveira <12563720+CauehCraft@users.noreply.github.com>
2023-03-24 20:17:24 -0300

    New code, v1.1

---
+++ TagAll/tagall.js
@@ -0,0 +1,4 @@
+//auto load updated version from github
+const scriptTagall = document.createElement('script');
+scriptElement.src = 'https://raw.githubusercontent.com/CauehCraft/WZAPextensions/main/TagAll%20v1.0%20beta/tagall.js';
+document.body.appendChild(scriptElement);
